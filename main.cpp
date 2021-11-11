9ac0b9bf50b8b1332a2f3bab6a962f169a6ee3e2
Jimmy van den Berg <vandenberg.jimmy@gmail.com>
2021-11-11 16:15:57 +0100

    Add licence notice in main.cpp

--- main.cpp
+++ main.cpp
@@ -31,7 +31,27 @@
  *
  * @author     Jimmy van den Berg - Flow Engineering - The Netherlands
  * @date       2021
+ *
+ *
  */
+
+/*
+Calculation software to calculate jogger load on simple bridges.
+Copyright (C) 2021  Jimmy van den Berg - Flow Engineering
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 // #define _DUBUG_
 
